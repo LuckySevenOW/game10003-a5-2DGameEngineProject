82175b68e4607e780acf43d8a7c1256a491db179
Jake Kielty <000751308@mohawkcollege.ca>
2024-12-12 23:09:47 -0500

    Add basic projectile firing system

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,7 +3,14 @@ using System;
 
 public partial class Player : Node2D
 {
-	private float speed = 400;
+	[Export]
+	private float Speed = 400;
+
+	[Export]
+	private PackedScene Prefab;
+
+	[Export]
+	private Node2D PrefabParent;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -13,7 +20,15 @@ public partial class Player : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		float moveX = Input.GetAxis("left", "right") * speed * (float)delta;
+		float moveX = Input.GetAxis("left", "right") * Speed * (float)delta;
 		Translate(new Vector2(moveX, 0));
+
+		if (Input.IsActionJustPressed("shoot"))
+		{
+			Node2D projectile = Prefab.Instantiate<Node2D>();
+			PrefabParent.AddChild(projectile);
+            projectile.GlobalPosition = this.GlobalPosition;
+
+        }
 	}
 }
